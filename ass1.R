104634f34c8f4bb570fc08278356a818196d6046
MonikaP <piechatzek@hotmail.com>
2020-01-22 19:23:21 -0800

    Added the autompg file and removed NAs

---
+++ ass1.R
@@ -0,0 +1,9 @@
+# Step 1: Insert the autompg file
+autompg = read.csv(file="autompg.ssv",sep=" ")
+# make sure you are in the correct directory on your computer -> use setwd("my/path/")
+# to View the csv -> View(autompg)
+
+# Step 2: Make sure you remove NA
+completeRows = complete.cases(autompg)
+autompg = autompg[completeRows,]
+dim(autompg)
