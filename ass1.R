d28227d234ec0d79a8386098289123d298129eda
MonikaP <piechatzek@hotmail.com>
2020-01-25 18:30:11 -0800

    Added questions 5, 6, 7, 8, 9, 10

--- ass1.R
+++ ass1.R
@@ -44,3 +44,46 @@ boxplot(autompg$lPer100km ~ autompg$cyl,
         ylab="Litres Per 100KM",
         xlab="Number of cylinders") 
 
+# Question 5
+blueOrange = blueOrange <-colorRampPalette(c("orange","blue"))
+
+#returns a customized function which takes parameters:
+
+# n = the number of (ordered) colours you want on the scale
+
+theColours = blueOrange(length(lPer100km)) #returns an actual collection of colours, for as many data points we have
+
+coloursToPaint = theColours[rank(lPer100km)] #orders the colours according to the rank of the fuel efficiency
+
+parcoord(autompg[,1:9],col=coloursToPaint)
+
+# Question 6
+
+# Vehicles that tend do have good fuel efficiency have low displ, low hp, low wt, and high modYr
+
+# Question 7
+
+# lPer100km and mpg has negative correlation
+# mpg and cylinder have negative correlation
+# displ and hp have positive correlation
+# hp and wt have positive correlation
+# wt and acc have negative correlation
+#
+# There are no other strong correlations with side-by-side variables
+
+# Question 8
+# Removed from assignment
+
+# Question 9
+# a) From the looks of the election map blue (right-wing) seems like it had the majority of wins
+
+# b) The winner is not as obvious in the third graph due to seemly equal amounts of red versus blue.
+
+# Question 10
+# An area would have grown in the third graph because the population in that area is quite dense 
+# whereas in an area that has shrunken means there is a low volume of people.
+
+# The color that occurs more in the third graph is red, which means that the areas where red
+# has more votes is where there is a higher population of people, even though it is a small area
+# physically. That is vice versa for blue areas, meaning blue covers more land physically but has less
+# density of people living in it.
