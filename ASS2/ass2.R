9af8354835e0116047153f02227904293546885d
MonikaP <piechatzek@hotmail.com>
2020-01-29 19:52:42 -0800

    added question 1 and part of 2, along with pdf answers

--- ASS2/ass2.R
+++ ASS2/ass2.R
@@ -1,3 +1,15 @@
 library(ggplot2)
 # Insert the mortality file
-mortality = read.csv(file="mortality.ssv",sep=" ")
+mortality = read.csv(file.choose())
+
+# Question 1
+plot(mortality[,1:2], type="l", main="Child Mortality Over Time")
+
+# Question 2
+
+# Create a new column with elderly probability
+mortality$old = with(mortality, 1 - mortality)
+
+# Question 3
+
+# Question 4
