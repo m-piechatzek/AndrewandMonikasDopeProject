6a836d8d9db65e0179bbe8b1ab28a43cd5625b92
MonikaP <piechatzek@hotmail.com>
2020-01-29 20:41:31 -0800

    Added part 2 of q2 but not sure if right

--- ASS2/ass2.R
+++ ASS2/ass2.R
@@ -9,6 +9,7 @@ plot(mortality[,1:2], type="l", main="Child Mortality Over Time")
 
 # Create a new column with elderly probability
 mortality$old = with(mortality, 1 - mortality)
+oldMortality = data.frame("X" = "P(X)", "02" = mortality[1, "mortality"], "70" = mortality[1, "old"])
 
 # Question 3
 
