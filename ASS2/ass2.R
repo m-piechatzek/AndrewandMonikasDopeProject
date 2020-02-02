5f4dfd7e955bc6594ca25ee43ab4984f5283c055
MonikaP <piechatzek@hotmail.com>
2020-02-02 15:47:58 -0800

    Added question 3

--- ASS2/ass2.R
+++ ASS2/ass2.R
@@ -10,7 +10,16 @@ plot(mortality[,1:2], type="l", main="Child Mortality Over Time")
 # Create a new column with elderly probability
 mortality$old = with(mortality, 1 - mortality)
 oldMortality = data.frame("X" = "P(X)", "02" = mortality[1, "mortality"], "70" = mortality[1, "old"])
+mean((oldMortality$X02[1]*2) + (oldMortality$X70[1]*70))
 
 # Question 3
+# Calculate the life expectancy
+mortality$lifeExpectancy = with(mortality, 70-68 * mortality$mortality)
+
+# View life expectancy in a table
+View(mortality$lifeExpectancy)
+
 
 # Question 4
+
+# Question 5
