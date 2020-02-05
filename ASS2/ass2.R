bbf1d17aa250e8cb24cf7de62e43b9bd9506d5a6
MonikaP <piechatzek@hotmail.com>
2020-02-04 19:33:22 -0800

    Adding photos and files

--- ASS2/ass2.R
+++ ASS2/ass2.R
@@ -26,11 +26,15 @@ View(mortality$lifeExpectancy)
 # Open File
 lungdata = read.csv(file.choose())
 
+# change 0 1 to smoker vs non smoker 
+fa = factor(lungdata$smoke, levels= c(0,1), labels = c("non-smoker","smoker"))
+boxplot(lungdata$fev~fa, ylab= "FEV in Litres", xlab="Non-smokers vs smokers", main="FEV for Non-smokers vs smokers")
+
 #Age compairison to Smoker vs non-smoker
-boxplot(lungdata$age~lungdata$smoke, ylab= "Age", xlab= "Non-Smoker vs Smoker", main = "Age compaired to Smoker vs Non-smoker" )
+boxplot(lungdata$age~fa, ylab= "Age", xlab= "Non-Smoker vs Smoker", main = "Age compaired to Smoker vs Non-smoker" )
 
 # Age comparision to FEV for smokers vs non-smokers
-ggplot(lungdata, aes(x=lungdata$age, y=lungdata$fev, color=lungdata$smoke)) + geom_point() + scale_color_brewer(palette = "Set1")
+ggplot(lungdata, aes(x=lungdata$age, y=lungdata$fev, color=fa)) + ggtitle("Comparing smokers vs non-smokers on FEV and age range") + xlab("Age") + ylab("FEV in Litres") + geom_point() + scale_color_brewer(palette = "Set1", name="Non-Smokers/Smokers")
 
 # Gender comparison within FEV and age
 ggplot(lungdata, aes(x=lungdata$age, y=lungdata$fev, color=lungdata$sex)) + geom_point() + scale_color_brewer(palette = "Set2")
@@ -38,8 +42,6 @@ ggplot(lungdata, aes(x=lungdata$age, y=lungdata$fev, color=lungdata$sex)) + geom
 # Height comparied to fev and smoke vs non
 
 ggplot(lungdata, aes(x=lungdata$ht, y=lungdata$fev, color=lungdata$smoke)) + geom_point() + scale_color_brewer(palette = "Set2")
-
-
 
 
 
