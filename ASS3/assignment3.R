9cb9262fa8ee8755bc9ab2711d378d65cba57df1
MonikaP <piechatzek@hotmail.com>
2020-02-22 21:00:55 -0800

    Added code Q4

--- ASS3/assignment3.R
+++ ASS3/assignment3.R
@@ -5,4 +5,7 @@ t.test(iris$Sepal.Width[iris$Species=="versicolor"])
 #3.
 t.test(iris$Sepal.Width[iris$Species=="virginica"],iris$Sepal.Width[iris$Species=="versicolor"], alternative = "two.sided")
 #4.
+iris2 = iris
+iris2$Sepal.Width = sample(iris2$Sepal.Width)
+t.test(iris2$Sepal.Width[iris2$Species=="virginica"],iris2$Sepal.Width[iris2$Species=="versicolor"])
 #5.
