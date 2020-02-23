2dc8698640619c94c91b25e0af2cbfb5b3a1a30c
MonikaP <piechatzek@hotmail.com>
2020-02-22 18:31:42 -0800

    Q3 R code

--- ASS3/assignment3.R
+++ ASS3/assignment3.R
@@ -3,5 +3,6 @@
 t.test(iris$Sepal.Width[iris$Species=="versicolor"])
 #2.
 #3.
+t.test(iris$Sepal.Width[iris$Species=="virginica"],iris$Sepal.Width[iris$Species=="versicolor"], alternative = "two.sided")
 #4.
 #5.
