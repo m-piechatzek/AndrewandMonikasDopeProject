7028bb0910b2bbb514eb15683d01d6c12a66e6fc
MonikaP <piechatzek@hotmail.com>
2020-02-23 15:12:23 -0800

    Q5 code

--- ASS3/assignment3.R
+++ ASS3/assignment3.R
@@ -9,3 +9,23 @@ iris2 = iris
 iris2$Sepal.Width = sample(iris2$Sepal.Width)
 t.test(iris2$Sepal.Width[iris2$Species=="virginica"],iris2$Sepal.Width[iris2$Species=="versicolor"])
 #5.
+summary(aov(Petal.Length ~ Species, data=iris.2))
+summary(aov(Sepal.Length ~ Species, data=iris.2))
+summary(aov(Petal.Width ~ Species, data=iris.2))
+summary(aov(Sepal.Width ~ Species, data=iris.2))
+
+# ANOVA 
+iris.2 = subset(iris, Species!="setosa")
+lmSepalWidth = lm(Sepal.Width ~ Species, data=iris.2)
+lmSepalLength = lm(Sepal.Length ~ Species, data=iris.2)
+lmPetalWidth = lm(Petal.Width ~ Species, data=iris.2)
+lmPetalLength = lm(Petal.Length ~ Species, data=iris.2)
+summary(lmSepalWidth)
+summary(lmSepalLength)
+summary(lmPetalWidth)
+summary(lmPetalLength)
+# TTest 
+t.test(iris.2$Sepal.Width[iris.2$Species=="virginica"],iris.2$Sepal.Width[iris.2$Species=="versicolor"], alternative = "two.sided")
+t.test(iris.2$Sepal.Length[iris.2$Species=="virginica"],iris.2$Sepal.Length[iris.2$Species=="versicolor"], alternative = "two.sided")
+t.test(iris.2$Petal.Width[iris.2$Species=="virginica"],iris.2$Petal.Width[iris.2$Species=="versicolor"], alternative = "two.sided")
+t.test(iris.2$Petal.Length[iris.2$Species=="virginica"],iris.2$Petal.Length[iris.2$Species=="versicolor"], alternative = "two.sided")
