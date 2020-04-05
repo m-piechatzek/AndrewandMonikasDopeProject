970e2c2c8dd6465eea3b13ec5c5761fd64059c38
MonikaP <piechatzek@hotmail.com>
2020-04-05 10:51:35 -0700

    Final project

---
+++ FinalProject/finalproject.R
@@ -0,0 +1,14 @@
+spotify_songs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv')
+nobel_winners <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-14/nobel_winners.csv")
+
+
+# Spotify Songs 2019
+
+# Association rules
+
+# Hierarchical Clustering
+
+
+# Nobel Prize Winners
+
+# Simple Linear Regression
