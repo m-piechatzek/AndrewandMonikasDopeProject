f98b2b720eb00d344ff143db412e5273f281973e
MonikaP <piechatzek@hotmail.com>
2020-04-05 21:44:15 -0700

    Added Association rules

--- FinalProject/finalproject.R
+++ FinalProject/finalproject.R
@@ -1,10 +1,19 @@
 spotify_songs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv')
 nobel_winners <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-14/nobel_winners.csv")
 
-
 # Spotify Songs 2019
 
 # Association rules
+
+# Get only key and popularity from dataset
+pop_key = select(spotify_songs, key, track_popularity)
+# convert to factors
+pop_key$key = as.factor(pop_key$key)
+pop_key$track_popularity = as.factor(pop_key$track_popularity)
+# apply aprior method
+key_pop_rules = apriori(pop_key, parameter = list(supp = 0.2, conf = 0.7))
+# show rules
+inspect(key_pop_rules)
 
 # Hierarchical Clustering
 
@@ -23,6 +32,7 @@ nobel_winners$new_birth[is.na(nobel_winners$new_birth)] = 0 # replace all NA wit
 nobel_winners$age_received = ifelse( nobel_winners$new_birth == 0, 0, nobel_winners$prize_year - nobel_winners$new_birth) 
 # Filter out only ages below 40
 filter_noble = filter(nobel_winners, age_received > 0 & age_received < 40)
-#
-abline(lm(prize_year ~ age_received, data = filter_noble, xlab = "Prize Year", ylab = "Age of Nominee", main="Simple Linear Regression on Young Adults receiving awards over time"), col = "red")
+# Plot the Linear Regression for Young Adults receiving awards over time
+plot(filter_noble$prize_year, filter_noble$age_received,xlab = "Prize Year", ylab = "Age of Nominee", main="Linear regression on young adults receiving awards over time")
+abline(lm(prize_year ~ age_received, data = filter_noble), col = "red")
 
